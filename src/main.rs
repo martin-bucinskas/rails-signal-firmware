3575c64528a3e0c49a0c8ec4dd609768544f4ab7
Martin <7595909+martin-bucinskas@users.noreply.github.com>
2022-05-16 00:17:33 +0100

    Creating a driver for status LEDs

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,29 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
-
     let mut led = pins.d13.into_output();
+    let mut red_led_driver = pins.d11.into_output();
+    let mut green_led_driver = pins.d10.into_output();
+    let mut blue_led_driver = pins.d9.into_output();
+    let mut count: i8 = 0;
 
     loop {
         led.toggle();
+
+        if count > 2 {
+            count = 0;
+        }
+
+        if count == 0 {
+            red_led_driver.toggle();
+        } else if count == 1 {
+            green_led_driver.toggle();
+        } else if count == 2 {
+            blue_led_driver.toggle();
+        }
+
+        count += 1;
+
         arduino_hal::delay_ms(1000);
     }
 }
